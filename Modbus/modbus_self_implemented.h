2a8aece7aa2ede2890fd1ce4a883994c3251cf19
SoerenToennesen <sorentoennesen@gmail.com>
2021-01-22 09:50:51 +0100

    small fixes

--- Modbus/modbus_self_implemented.h
+++ Modbus/modbus_self_implemented.h
@@ -12,7 +12,8 @@ struct response entry_func_dmamode(uint8_t **RTS_code, uint8_t count);
 struct response entry_func_regmode(uint8_t *RTS_code, uint8_t count);
 
 //extern volatile int16_t HoldingRegister[4096];
-volatile int16_t HoldingRegister[7422];
+//extern volatile int16_t HoldingRegister[7422];
+extern volatile int16_t HoldingRegister[6144];
 //extern volatile uint16_t HoldingRegister_RTS[1000];
 //extern volatile uint16_t HoldingRegister_MTC_X[4096]; //can't do one for each because data overflows in cypress fx3...
 //extern volatile uint16_t HoldingRegister_MTC_Y[4096];
